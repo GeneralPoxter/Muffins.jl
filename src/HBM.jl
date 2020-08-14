d10ddcff570fe3d3acde8b97d05e610f1f4cf20f
Antara Hebbar <antara.hebbar@gmail.com>
2020-08-13 23:45:18 -0400

    small changes

--- src/HBM.jl
+++ src/HBM.jl
@@ -23,25 +23,28 @@ k = 2 to get f(25; 22)  23
 
 #Determines an upper bound using Hard-Buddy Match methon
 #Author: Antara Hebbar
-function hbm(m::Int64, s::Int64, output::Int64=0)
 
+function hbm(m::Int64, s::Int64, output::Int64=1)
+
+output>0&&printHeader(center("HARD-BUDDY MATCH METHOD"))
 
 if m<s||m<=0||s<=0
-    if output>1
+    if output>0
         printf("HBM method does not apply", line=true)
         printEnd()
     end
     return Nothing
 elseif m%s==0
-    if output>1
-        printf("Since $m % $s = 0, f($m, $s) = 1")
+    if output>0
+        printf("Since $m % $s = 0, f($m, $s) = 1.")
+        printEnd()
     end
     return 1
 else
 
 V = Int64(ceil(2m/s))
 
-if V==3
+    if V==3
 
 #important variables for algorithm hbm
 d = m-s
@@ -51,24 +54,44 @@ while 3*d*i < s
 end
 k = i-1
 a = s-(3*d*k)
-
 Xsolutions = Xsol(a,d,k)
 
-if length(Xsolutions)==0
+        if length(Xsolutions)==0
+            return 1
+        elseif a==(2*d)
+            alpha = fc(m,s, output=0)
+            if output>0
+                printf("Since a=2d, hbm($m, $s) = fc($m, $s).
+                Therefore, upperbound of hbm($m, $s) is $alpha.")
+                printEnd()
+            end
+            return alpha
+        else
+            X=minimum(Xsolutions)
+
+            #special cases
+            if a==10&&d==7&&m==(21*k+17)&&s==(21*k+10)
+                alpha=(7*k+4)//(21*k+10)
+            elseif a==4&&d==3&&m==(9*k+7)&&s==(9*k+4)
+                alpha=(9*k+5)//(27*k+12)
+            else
+                alpha=(d*k+X)//(3*d*k+a)
+            end
+
+            if output>0
+                printfT("Deriving alpha", "The derived value for X is $X. f($m, $s) ≤ (dk+X)/(3dk+a).", "",
+                "Once plugging in d, k, a, and X, the upper bound of hbm($m, $s) is $alpha.")
+                printEnd()
+            end
+        return alpha, a, d, k, X
+        end
+
+    else
+        if output>0
+            printf("Hard-buddy match method requires that students are given 3-shares and 2-shares.
+            Since in f($m, $s), students are given $V and $(V-1) shares, HBM does not apply here.")
+        end
     return 1
-else
-
-X=minimum(Xsolutions)
-alpha = (d*k+X)//(3*d*k+a)
-return alpha
-end
-
-else
-    if output>0
-        printf("Hard-buddy match method requires that students are given 3-shares.
-        Since in f($m, $s), students are given $V shares, HBM does not apply here.")
-    end
-    return 1
 end
 
 end
@@ -76,25 +99,77 @@ end
 end
 
 
-#Verifies that with an input of a,d,k,X, f(3dk+a+d, 3dk+a) ≤ (dk+X)/(3dk+a)
-function VHBM(X,a,d,k)
+#Verifies that with an input of a,d,k,X, f(3dk+a+d, 3dk+a) ≤ (dk+X)/(3dk+a), ouputs proof if wanted
+function VHBM(X,a,d,k, output::Int64=2)
 
+output>0&&printHeader(center("HARD-BUDDY MATCH PROOF"))
 #pre-processing stage: checks for a bad input
+pre=true
+alpha=(d*k+X)//(3*d*k+a)
+muffins = 3*d*k+a+d
+students=3*d*k+a
+V = Int64(ceil(2*muffins/students))
+print(muffins, students, V)
 
-pre=false
+#formatting
+alphaS=formatFrac(alpha)
+
 if a<1 || a>(3*d)
+    if output>0
+        printf("Bad input, a is less than 1 or greater than 3d ($(3*d)).")
+        printEnd()
+    end
     return false
 elseif a==(2*d) #use FC
+    alpha=fc(muffins, students)
     pre=true
 elseif X<a//3
+    if output>0
+        printf("Bad input, X is less than a/3 ($(a//3)).")
+        printEnd()
+    end
     return false
-elseif X>=a//3 && a>(2*d+1) && a<(3*d-1) #theorem 10.11.1 in muffins book,
-    pre=true #now we assume that a is in that interval
+elseif X>=a//3 && a>(2*d+1) && a<(3*d-1) #theorem 10.11.1 in muffins book, we assume a is in that interval
+    pre=true
 elseif X>=(a//2) #theorem 10.11.2, we assume X<a/2
     pre=true
 elseif X>=(a+d)//4 #theorem 10.11.3, we assume X<(a+d)/4
     pre=true
 end
+
+if pre&&V==3
+
+    if output>1 #start of proof
+        printHeader("CLAIM:")
+        printf("Assume, by way of contradiction, there is a ($muffins, $students) procedure where the smallest piece is > $alphaS.", "",
+        "We are looking for a contradiction.")
+
+        printHeader("SOLVING FOR SHARES:")
+        printfT("Solving for students and shares", "Every student has $(V) or $(V-1) shares.", "", "Let s₂ and s₃ equal the number of 2 and 3-students.",
+        "", "s₂ = $(3*d*k+a-2*d). There are $(6*d*k+2*a-4*d) 2-shares.", "", "
+        s₃ = $(2*d). There are $(6*d) 3-shares.")
+
+        printHeader("DEFINING TERMS FOR ALGORITHM: ")
+        printfT("Variables", "We will use the following variables for the HBM algorithm (where m=muffins and s=students):", "", "d = m-s = $d", "",
+        "k is the largest number >= 0 such that 3dk > s. k = $k.", "", "a = s - 3dk = $a")
+        printfT("Common Terms", "A buddy of share size x is equal to 1-x. A match of share size y is equal to $muffins/$students-y.")
+    end
+
+
+
+
+
+
+
+else
+    if output>0
+        printf("Vhbm failed, hbm method needs students to have $V or $(V-1) shares.")
+        printEnd()
+    end
+end
+
+
+
 end
 
 
@@ -102,8 +177,6 @@ end
 
 function Xsol(a,d,k)
 Xsolutions = Array{Rational}(undef, 0)
-
-#if pre
 
 if d>=1 && k>=1 && a >= 1 && a <= (2*d) #11.5.1: y₁₁₃ permitted, all other variables forbidden
 
